35c692c1817666a45f6e1870a11a6624b74ef2fa
NoZA-24 <63103448+NoZA-24@users.noreply.github.com>
2020-04-08 02:58:46 +0900

    Update synch.c

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -67,11 +67,10 @@ sema_down (struct semaphore *sema)
 
   old_level = intr_disable ();
   while (sema->value == 0) 
-    {
-      list_insert_ordered (&sema->waiters, &thread_current ()->elem,
-		cmp_priority,0);
-      thread_block ();
-    }
+  {
+    list_insert_ordered (&sema->waiters, &thread_current ()->elem,cmp_priority,0);
+    thread_block ();
+  }
   sema->value--;
   intr_set_level (old_level);
 }
